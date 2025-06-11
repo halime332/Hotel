803f5fd830990255c7c10ce364a63b141e4321d7
halime332 <halimeyavuz1001@gmail.com>
2025-06-11 21:03:00 +0300

    hotel

--- frontend/src/pages/detail/index.tsx
+++ frontend/src/pages/detail/index.tsx
@@ -1,23 +1,49 @@
 import { useQuery } from "@tanstack/react-query";
-import type { FC } from "react";
+import { Children, type FC } from "react";
 import { useParams } from "react-router-dom";
 import { getPlace } from "../../utils/servis";
+import Loader from "../../components/loader";
+import Error from "../../components/error";
+import Return from "./return";
 
 
 const Detail:FC = () => {
   const {id}=useParams();
 
-   const {isLoading,error,data}=useQuery({
+   const {isLoading,error,data,refetch}=useQuery({
      queryKey:["place"],
      queryFn:()=>getPlace(id as string),
    });
 
    
-   console.log(isLoading,error,data);
+  if(isLoading)
+    return(
+    <Children>
+      <Loader/>
+    </Children>
+      
+  
+  );
 
+  if(error)
+    return(<div>
+      <Return/>
+      <Error  info={error} refetch={refetch}/>
+    </div>
+  );
+
+
+  if(!data)  return;
+  
   return (
-    <div>
-      Detail
+  
+    <div className="container">
+        <Return/>
+     
+     <div>
+      <h1>{data.name}</h1>
+    </div>
+
     </div>
   )
 };
